ab8722dc04d72896ad1667d94529da06743e1eb3
AlekseiKromski <Aleksei.Kromski@ivkhk.ee>
2019-12-04 09:40:34 +0200

    12/4/2019 - Primary Key/Default

--- SQL_request/November/11.27.2019_createDatabase.sql
+++ SQL_request/November/11.27.2019_createDatabase.sql
@@ -7,11 +7,31 @@ USE projects_AK
 
 --System requests
 SELECT * FROM product
+SELECT * FROM customer
 
 --Create tables
+--v1
 CREATE TABLE product(product_no INTEGER IDENTITY(10000, 1) NOT NULL, product_name CHAR(30) NOT NULL, price MONEY) --(Identity - автоматом заполняет)
+--v2
+CREATE TABLE product(product_no INTEGER IDENTITY(10000, 1) NOT NULL, product_name CHAR(30) NOT NULL, price MONEY, date_reg DATETIME DEFAULT Getdate()) --Если мы ничего не вставляем в столбец date_reg, то он ставит нынешнюю
+--v3
+CREATE TABLE product(product_no INTEGER IDENTITY(10000, 1) NOT NULL CONSTRAINT PK_product_no PRIMARY KEY, product_name CHAR(30) NOT NULL, price MONEY, date_reg DATETIME DEFAULT Getdate()) --Выставляем PK
+CREATE TABLE customer(cust_no INTEGER IDENTITY(10000, 1) NOT NULL CONSTRAINT PK_cust_no PRIMARY KEY, cust_group CHAR(3) NULL, CHECK (cust_group IN ('c1','c2','c10')))
 
 --Isert data to tables 
+--v1
 INSERT INTO product (product_name,price) VALUES('Iphone',999)
 INSERT INTO product (product_name,price) VALUES('Xiaomi',120)
 INSERT INTO product (product_name,price) VALUES('Samsung',800)
+--v2
+--Data for product
+	INSERT INTO product (product_name,price,date_reg) VALUES('Samsung',800,DEFAULT)
+	INSERT INTO product (product_name,price,date_reg) VALUES('Motorolla',NULL,'2019-12-05 06:08:58.555')
+--Data for customer
+	INSERT INTO customer VALUES('c2')
+	INSERT INTO customer VALUES('c10')
+	INSERT INTO customer VALUES('c5') -- !!! Это значение не вставиться из-за (CHECK) !!!
+
+--drop table
+DROP TABLE product
+DROP TABLE customer
