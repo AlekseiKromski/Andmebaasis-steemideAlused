171490618fdda3e938282c71f9a0cb35ca62a191
AlekseiKromski <Aleksei.Kromski@ivkhk.ee>
2019-12-04 11:15:23 +0200

    12/4/2019 - Foreign key / Check / default

--- SQL_request/November/11.27.2019_createDatabase.sql
+++ SQL_request/November/11.27.2019_createDatabase.sql
@@ -8,6 +8,7 @@ USE projects_AK
 --System requests
 SELECT * FROM product
 SELECT * FROM customer
+SELECT * FROM sales
 
 --Create tables
 --v1
@@ -17,13 +18,20 @@ CREATE TABLE product(product_no INTEGER IDENTITY(10000, 1) NOT NULL, product_nam
 --v3
 CREATE TABLE product(product_no INTEGER IDENTITY(10000, 1) NOT NULL CONSTRAINT PK_product_no PRIMARY KEY, product_name CHAR(30) NOT NULL, price MONEY, date_reg DATETIME DEFAULT Getdate()) --¬ыставл€ем PK
 CREATE TABLE customer(cust_no INTEGER IDENTITY(10000, 1) NOT NULL CONSTRAINT PK_cust_no PRIMARY KEY, cust_group CHAR(3) NULL, CHECK (cust_group IN ('c1','c2','c10')))
+CREATE TABLE sales(
+	sales_no INTEGER IDENTITY(10000, 1) CONSTRAINT PK_sales_no PRIMARY KEY,
+	product INTEGER NOT NULL,
+	count_product INTEGER DEFAULT 1 NOT NULL,
+	sales_ClearanceDate DATETIME DEFAULT Getdate() NOT NULL,
+	treatmentDate DATETIME DEFAULT Getdate() + 3, CHECK (treatmentDate >= sales_ClearanceDate),
+	CONSTRAINT FK_product FOREIGN KEY(product) REFERENCES product (product_no)) 
 
 --Isert data to tables 
 --v1
 INSERT INTO product (product_name,price) VALUES('Iphone',999)
 INSERT INTO product (product_name,price) VALUES('Xiaomi',120)
 INSERT INTO product (product_name,price) VALUES('Samsung',800)
---v2
+--v2/v3
 --Data for product
 	INSERT INTO product (product_name,price,date_reg) VALUES('Samsung',800,DEFAULT)
 	INSERT INTO product (product_name,price,date_reg) VALUES('Motorolla',NULL,'2019-12-05 06:08:58.555')
@@ -31,7 +39,17 @@ INSERT INTO product (product_name,price) VALUES('Samsung',800)
 	INSERT INTO customer VALUES('c2')
 	INSERT INTO customer VALUES('c10')
 	INSERT INTO customer VALUES('c5') -- !!! Ёто значение не вставитьс€ из-за (CHECK) !!!
+--Data for sales
+	INSERT INTO sales (product,count_product,sales_ClearanceDate,treatmentDate) VALUES (10001,10,DEFAULT,DEFAULT)
 
 --drop table
 DROP TABLE product
 DROP TABLE customer
+DROP TABLE sales
+
+--query
+
+SELECT sales.sales_no,sales.product,sales.count_product, product.product_name, product.price, sales.count_product * product.price AS earned 
+FROM sales JOIN product ON sales.product = product.product_no
+
+SELECT SUM(sales.count_product * product.price) AS earned, COUNT(sales.count_product) as CountSales FROM sales JOIN product ON sales.product = product.product_no --ѕоказывает, сколько всего заработано и коль.-во товара
