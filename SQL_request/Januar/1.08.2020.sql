e48ac677f8086684825425fb44fca12c3b2ee918
AlekseiKromski <Aleksei.Kromski@ivkhk.ee>
2020-01-08 11:04:24 +0200

    1/8/2020 - add new SQL requests

--- SQL_request/Januar/1.08.2020.sql
+++ SQL_request/Januar/1.08.2020.sql
@@ -21,3 +21,13 @@ SELECT ProductID, ProductName, UnitsInStock, CAST(ProductID AS CHAR(5)) + Produc
 SELECT UnitPrice, CONVERT(DECIMAL(10,1), UnitPrice) FROM Products
 
 SELECT CAST(UnitPrice AS DECIMAL(10,1)) FROM Products
+
+--Maket for site 
+SELECT * FROM works_on
+SELECT * FROM employee
+SELECT * FROM project
+
+SELECT project.project_name,works_on.project_no,COUNT(*) FROM works_on 
+INNER JOIN project ON works_on.project_no = project.project_no 
+GROUP BY works_on.project_no, project.project_name
+
