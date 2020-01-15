ac6d8fa959888020f59fee2961712c5e9a78afc8
AlekseiKromski <Aleksei.Kromski@ivkhk.ee>
2020-01-15 09:48:45 +0200

    1.15.2020 -2

--- SQL_request/Januar/1.15.2020.sql
+++ SQL_request/Januar/1.15.2020.sql
@@ -13,7 +13,7 @@ SELECT project_no, COUNT(*) FROM works_on GROUP BY project_no
 SELECT employee.emp_no, emp_lname, emp_fname FROM employee INNER JOIN works_on ON employee.emp_no = works_on.emp_no 
 
 
-
+--IF
 GO
 DECLARE @project varchar(3)
 SET @project = 'p1'
@@ -29,5 +29,31 @@ ELSE
 		PRINT 'The following employees work for the project' + @project
 		SELECT emp_lname, emp_fname 
 		FROM employee INNER JOIN works_on ON employee.emp_no = works_on.emp_no  WHERE project_no = @project
+	END
+GO
+
+SELECT * FROM project
+UPDATE project SET budget = 1
+UPDATE project SET budget = budget - 150 WHERE project_no IN ('p1', 'p2','p4')
+UPDATE project SET budget = budget / 100 WHERE project_no IN ('p3', 'p2','p5')
+
+--WHILE 
+GO
+DECLARE @sum1 money
+DECLARE @sum2 money
+SET @sum1 = 500000
+SET @sum2 = 240000
+
+WHILE (SELECT SUM(budget) FROM project) < @sum1
+	BEGIN
+		UPDATE project SET  budget = budget*1.1
+		PRINT 'Change budget' 
+		IF (SELECT MAX(budget) FROM project) > @sum2
+			BEGIN
+				SELECT MAX(budget) FROM project
+				BREAK
+			END
+		ELSE			
+			CONTINUE
 	END
 GO
