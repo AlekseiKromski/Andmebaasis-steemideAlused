7c848504c4e08fd2dca6aa5f67e84cde88beab1b
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2020-02-19 11:28:08 +0200

    test

--- SQL_request/Februar/19.02.2020.sql
+++ SQL_request/Februar/19.02.2020.sql
@@ -11,8 +11,8 @@ GO
 GO
 
 SELECT SUM(budget) FROM project
-
-SELECT dbo.compute_costs(25)
+   
+SELECT dbo.compute_costs(25)  
 SELECT dbo.compute_costs(5)
 SELECT dbo.compute_costs(10)
 
@@ -37,7 +37,6 @@ GO
 	CREATE FUNCTION emp(@emp_no VARCHAR(20))
 	RETURNS @emp TABLE (emp_no INT, FaL_name VARCHAR(20), count_project VARCHAR(20))
 		BEGIN
-			IF (SELECT emp_no FROM employee WHERE emp_no = '9999')
 			IF @emp_no = 'All'
 				BEGIN
 					INSERT INTO @emp 
@@ -60,3 +59,38 @@ DROP FUNCTION emp
 
 SELECT * FROM dbo.emp('All')
 
+CREATE TABLE date_last(date_cur DATE)
+
+INSERT INTO date_last VALUES ('2021-02-19')
+
+SELECT *, YEAR(date_cur) as year,MONTH(date_cur) as month FROM date_last
+
+SELECT *, YEAR(date_cur) as year,MONTH(date_cur) as month, datefromparts(YEAR(date_cur),MONTH(date_cur),
+	CASE
+		WHEN MONTH(date_cur) IN (1,3,5,7,8,10,12) THEN 31
+		WHEN MONTH(date_cur) IN (4,6,9,11) THEN 30
+		WHEN (YEAR(date_cur) % 4 = 0 OR YEAR(date_cur) % 400 = 0) AND YEAR(date_cur) % 100 != 0 THEN 29
+		ELSE 28
+	END) AS lastDay
+FROM date_last
+
+
+GO
+CREATE FUNCTION lastDay(@date DATE)
+RETURNS @latDay DATE
+	BEGIN
+	SELECT @latDay = 
+		 datefromparts(YEAR(@date),MONTH(@date),
+			CASE
+				WHEN MONTH(@date) IN (1,3,5,7,8,10,12) THEN 31
+				WHEN MONTH(@date) IN (4,6,9,11) THEN 30
+				WHEN (YEAR(@date) % 4 = 0 OR YEAR(@date) % 400 = 0) AND YEAR(@date) % 100 != 0 THEN 29
+				ELSE 28
+			END) 
+		RETURN 
+	END
+GO
+
+DROP FUNCTION lastDay
+
+SELECT dbo.lastDay('2020-02-19')
