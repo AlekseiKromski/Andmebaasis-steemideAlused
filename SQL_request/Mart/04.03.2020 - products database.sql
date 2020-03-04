cfbda725f2f89b2f0b857969a8b9c430bf153bd8
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2020-03-04 09:46:19 +0200

    04.03.2020 - merge/products(warehouse) database 2

--- SQL_request/Mart/04.03.2020 - products database.sql
+++ SQL_request/Mart/04.03.2020 - products database.sql
@@ -15,3 +15,8 @@ INSERT INTO warehouse VALUES (1,'Milk Happy',0.80,60),(2,'Coca-cola',0.50,40),(3
 
 INSERT INTO orderProduct VALUES (1,'Milk Happy',0.80,60),(2,'Coca-cola',0.50,40),(3,'Sousage',0.30,100),(4,'Crisps',1.60,600)
 
+MERGE warehouse AS Target USING orderProduct AS Source ON (Target.productId = Source.productId)
+	WHEN MATCHED 
+		THEN UPDATE SET productCount = Source.productCount
+	WHEN NOT MATCHED
+		THEN INSERT VALUES (Source.productId,Source.productName,Source.productPrice,Source.productCount)
